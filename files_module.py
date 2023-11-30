a080458ffcc0637d0414186ceae1976483c24b4d
roman-ks <80638824+roman-ks@users.noreply.github.com>
2023-12-01 00:28:12 +0200

    recalculate hash on each invocation because file might have changed

--- files_module.py
+++ files_module.py
@@ -27,14 +27,12 @@ class File:
         self.sha256 = None
 
     def get_sha256(self):
-        if not self.sha256:
-            sha256_hash = hashlib.sha256()
+        sha256_hash = hashlib.sha256()
 
-            with open(self.path, "rb") as file:
-                for byte_block in iter(lambda: file.read(4096), b""):
-                    sha256_hash.update(byte_block)
-            self.sha256 = sha256_hash.hexdigest()
-        return self.sha256
+        with open(self.path, "rb") as file:
+            for byte_block in iter(lambda: file.read(4096), b""):
+                sha256_hash.update(byte_block)
+        return sha256_hash.hexdigest()
 
     def exists(self):
         return os.path.exists(self.path)
