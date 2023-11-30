3d01d78c17bb8a96be427227048329393b66ecb9
roman-ks <80638824+roman-ks@users.noreply.github.com>
2023-12-01 00:14:47 +0200

    add initial backuper script

---
+++ files_module.py
@@ -0,0 +1,43 @@
+import hashlib
+import os
+
+
+def _get_root(name):
+    if os.name == 'nt':
+        return name + "\\"
+
+
+class Drive:
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.root = _get_root(name)
+
+    def __str__(self):
+        return f'Drive(name={self.name}, id={self.id})'
+
+
+class File:
+
+    def __init__(self, name, drive):
+        self.name = name
+        self.drive = drive
+        self.path = os.path.join(drive.root, name)
+        self.sha256 = None
+
+    def get_sha256(self):
+        if not self.sha256:
+            sha256_hash = hashlib.sha256()
+
+            with open(self.path, "rb") as file:
+                for byte_block in iter(lambda: file.read(4096), b""):
+                    sha256_hash.update(byte_block)
+            self.sha256 = sha256_hash.hexdigest()
+        return self.sha256
+
+    def exists(self):
+        return os.path.exists(self.path)
+
+    def get_last_modified(self):
+        return os.path.getmtime(self.path)
---
+++ main.py
@@ -0,0 +1,55 @@
+import datetime
+import os
+import shutil
+import string
+from files_module import Drive, File
+
+rel_file_path = 'backup/MainDatabase.kdbx'
+
+
+def get_drives():
+    drives_list = []
+    if os.name == 'nt':  # win
+        for letter in string.ascii_uppercase:
+            drive_path = letter + ":\\"
+            if os.path.exists(drive_path):
+                drives_list.append(Drive(str(os.stat(drive_path).st_dev) + '-win', letter + ':'))
+
+    return drives_list
+
+
+def get_abs_file_paths(drives_list):
+    return [os.path.join(drive.root, rel_file_path) for drive in drives_list]
+
+
+def get_newest_file(files):
+    existent_files = filter(lambda x: x.exists(), files)
+    return next(iter(sorted(existent_files, key=lambda x: x.get_last_modified())))
+
+
+drives_list = get_drives()
+drives_str = ', '.join(map(str,drives_list))
+print(f'Found {len(drives_list)} drives: [{drives_str}]')
+
+files = [File(rel_file_path, drive) for drive in drives_list]
+
+existent_files = filter(lambda x: x.exists(), files)
+if existent_files:
+    newest_file = get_newest_file(files)
+    newest_date = datetime.datetime.fromtimestamp(newest_file.get_last_modified())
+    print(f'Newest file {newest_file.path}({newest_date.strftime("%Y-%m-%d %H:%M:%S")})')
+
+    for file in files:
+        if file != newest_file:
+            if not file.exists() or (file.exists() and newest_file.get_sha256() != file.get_sha256()):
+                os.makedirs(os.path.dirname(file.path), exist_ok=True)
+                shutil.copy(newest_file.path, file.path)
+
+                file_hash = file.get_sha256()
+                print(
+                    f'Copied to drive: {file.drive.name}, id:{file.drive.id}, abs path: {file.path}, hash:{file_hash}')
+            else:
+                print(f'File {file.path} up to date')
+
+else:
+    print("File not found on any drive")
