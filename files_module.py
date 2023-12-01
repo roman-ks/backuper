0e635ae8c76d249bcd38fcddffafe499683169e1
roman-ks <80638824+roman-ks@users.noreply.github.com>
2023-12-02 00:38:47 +0200

    load configs from file. Sync multiple files in folder. Nested folders not supported for now

--- files_module.py
+++ files_module.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+import datetime
 
 
 def _get_root(name):
@@ -9,21 +10,26 @@ def _get_root(name):
 
 class Drive:
 
-    def __init__(self, id, name):
+    def __init__(self, id, sys_name):
         self.id = id
-        self.name = name
-        self.root = _get_root(name)
+        self.sys_name = sys_name
+        self.root = _get_root(sys_name)
+        self.name = None
+        self.folder_path = None
 
     def __str__(self):
-        return f'Drive(name={self.name}, id={self.id})'
+        return f'Drive(sys_name={self.sys_name}, id={self.id}, name={self.name})'
+
+    def __eq__(self, other):
+        return self.id == other.id
 
 
 class File:
 
-    def __init__(self, name, drive):
-        self.name = name
+    def __init__(self, rel_path, drive):
         self.drive = drive
-        self.path = os.path.join(drive.root, name)
+        self.rel_path = rel_path
+        self.path = os.path.join(drive.folder_path, rel_path)
 
     def get_sha256(self):
         sha256_hash = hashlib.sha256()
@@ -38,3 +44,10 @@ class File:
 
     def get_last_modified(self):
         return os.path.getmtime(self.path)
+
+    def get_str_last_modified(self, dateformat="%Y-%m-%d %H:%M:%S"):
+        date = datetime.datetime.fromtimestamp(self.get_last_modified())
+        return date.strftime(dateformat)
+
+    def __eq__(self, other):
+        return self.get_sha256() == other.get_sha256()
--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-import datetime
 import os
 import shutil
 import string
+import json
+import glob
+
 from files_module import Drive, File
 
-rel_file_path = 'backup/MainDatabase.kdbx'
+config = {}
+drive_name_file = ''
+rel_folder_path = ''
+includes_pathname = ''
+
+
+def load_config():
+    with open('backuper_config.json', 'r') as conf_file:
+        global config
+        config = json.load(conf_file)
+
+    global drive_name_file
+    drive_name_file = config.get("files", {}).get("drive_name_file", None)
+    global rel_folder_path
+    rel_folder_path = config["folder_path"]
+    global includes_pathname
+    includes_pathname = config.get("files", {}).get("includes", None)
 
 
 def get_drives():
@@ -15,41 +33,74 @@ def get_drives():
             if os.path.exists(drive_path):
                 drives_list.append(Drive(str(os.stat(drive_path).st_dev) + '-win', letter + ':'))
 
+    if drive_name_file:
+        for drive in drives_list:
+            drive_name_file_path = os.path.join(drive.root, rel_folder_path, drive_name_file)
+            if os.path.exists(drive_name_file_path) and os.path.isfile(drive_name_file_path):
+                with open(drive_name_file_path, "r") as name_file:
+                    name = name_file.read()
+                    drive.name = name
+
+    for drive in drives_list:
+        drive.folder_path = os.path.join(drive.root, rel_folder_path)
+
     return drives_list
 
 
-def get_abs_file_paths(drives_list):
-    return [os.path.join(drive.root, rel_file_path) for drive in drives_list]
+def get_files_on_drive(drive):
+    paths = glob.glob(os.path.join(drive.folder_path, includes_pathname))
+
+    drive_name_file_on_this_drive = os.path.join(drive.folder_path, drive_name_file)
+    if drive_name_file_on_this_drive in paths:
+        paths.remove(os.path.join(drive.folder_path, drive_name_file))
+
+    return [File(os.path.relpath(path, drive.folder_path), drive) for path in paths]
+
+
+def get_unique_paths():
+    rel_paths = set({})
+    for drive in drives_list:
+        files = get_files_on_drive(drive)
+
+        [rel_paths.add(file.rel_path) for file in files]
+
+    return rel_paths
 
 
 def get_newest_file(files):
     existent_files = filter(lambda x: x.exists(), files)
-    return next(iter(sorted(existent_files, key=lambda x: x.get_last_modified())))
+    return next(iter(sorted(existent_files, key=lambda x: x.get_last_modified(), reverse=True)))
 
+
+load_config()
+if not config.get('folder_path', None):
+    print("folder_path not set in config!")
+    exit(0)
 
 drives_list = get_drives()
-drives_str = ', '.join(map(str,drives_list))
+drives_str = ', '.join(map(str, drives_list))
 print(f'Found {len(drives_list)} drives: [{drives_str}]')
 
-files = [File(rel_file_path, drive) for drive in drives_list]
+unique_files = get_unique_paths()
+print(f"Found unique files: {unique_files}")
+for unique_path in unique_files:
+    # list files with the same path on different drives
+    files_on_drives = []
+    for drive in drives_list:
+        files_on_drives.append(File(unique_path, drive))
 
-existent_files = filter(lambda x: x.exists(), files)
-if existent_files:
-    newest_file = get_newest_file(files)
-    newest_date = datetime.datetime.fromtimestamp(newest_file.get_last_modified())
-    print(f'Newest file {newest_file.path}({newest_date.strftime("%Y-%m-%d %H:%M:%S")})')
-
-    for file in files:
-        if file != newest_file:
-            if not file.exists() or (file.exists() and newest_file.get_sha256() != file.get_sha256()):
-                os.makedirs(os.path.dirname(file.path), exist_ok=True)
-                shutil.copy2(newest_file.path, file.path)
-
-                file_hash = file.get_sha256()
-                print(
-                    f'Copied to drive: {file.drive.name}, id:{file.drive.id}, abs path: {file.path}, hash:{file_hash}')
+    # get newest
+    newest_file = get_newest_file(files_on_drives)
+    print(f"Newest {unique_path}: {newest_file.get_str_last_modified()}")
+    # copy newest to all drives
+    for drive in drives_list:
+        file_on_this_drive = File(newest_file.rel_path, drive)
+        if drive != newest_file.drive:
+            if file_on_this_drive.exists() and file_on_this_drive == newest_file:
+                print(f"File {file_on_this_drive.path} is up to date")
             else:
-                print(f'File {file.path} up to date')
-
-else:
-    print("File not found on any drive")
+                os.makedirs(os.path.dirname(file_on_this_drive.path), exist_ok=True)
+                shutil.copy2(newest_file.path, file_on_this_drive.path)
+                print(f'Copied {newest_file.path} to {file_on_this_drive.path}')
+        else:
+            print(f"File {file_on_this_drive.path} is up to date")
